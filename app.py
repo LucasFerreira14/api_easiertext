50ab384de1e5d08efd4fb9a365b36bc6adc2f058
LucasFerreira14 <ferreira.silva5641@gmail.com>
2022-05-26 15:45:15 -0300

    third version

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify, url_for, redirect
+from flask import Flask, jsonify, url_for, redirect, request, Response
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects import postgresql
-import psycopg2
+from flask_marshmallow import Marshmallow
+
+# from sqlalchemy.dialects import postgresql
+# import psycopg2
 
 app = Flask("__name__")
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://znsnbvxrrxdwfb:8d7d8a68a323e888d76cbe01ef24bf39fd3927882d362cb7c0e15b24ef558832@ec2-52-3-200-138.compute-1.amazonaws.com:5432/dahehqr4qgkurb"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://znsnbvxrrxdwfb:8d7d8a68a323e888d76cbe01ef24bf39fd3927882d362cb7c0e15b24ef558832@ec2-52-3-200-138.compute-1.amazonaws.com:5432/dahehqr4qgkurb"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 # Modelagem Entidade
@@ -20,17 +23,59 @@ class Texto(db.Model):
         self.title = title
         self.text = text
 
+class TextoSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Texto
+
+
 # Criação de Rotas
 
 @app.route("/")
 def index():
-    return redirect(url_for("texts"))   
+    return redirect(url_for("showAllTexts"))
 
-@app.route("/texts", methods=["GET"])
+
+@app.route("/texts", methods=["GET", "DELETE"])
 def showAllTexts():
-    query = Texto.query.all()
-    return jsonify(query)
+    texts = Texto.query.all()
+    texto_schema = TextoSchema(many=True)
+    result = texto_schema.dump(texts)
+    return jsonify(result)
+
+
+@app.route("/texts/add/", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        data = request.json
+        text = Texto(data["title"], data["text"])
+        db.session.add(text)
+        db.session.commit()
+        return jsonify(data)
+    return render_template("add.html")
+
+
+@app.route("/texts/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    text = Texto.query.get(id)
+    if request.method == "POST":
+        data = request.json
+        text.title = data["title"]
+        text.text = data["text"]
+        db.session.add(text)
+        db.session.commit()
+        return Response().status_code
+    return render_template("edit.html", cliente=cliente)
+
+@app.route("/texts/delete/<int:id>", methods=["GET", "DELETE"])
+def delete(id):
+    text = Texto.query.get(id)
+    db.session.delete(text)
+    db.session.commit()
+    if Response().status_code == 200:
+        return redirect(url_for("showAllTexts"))
+
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run()
